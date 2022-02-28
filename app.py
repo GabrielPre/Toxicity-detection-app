de61b3de7f42ff3ce6041b5488e0212255f54970
Louis Tardy <louis.tardy@efrei.net>
2022-02-28 15:39:28 +0100

    First front end commit, Docker compose available
    Files only for front end, possible changes coming afterwards

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask.wrappers import Request
+from flask import Flask, request, jsonify
+#from Ml_model import predict
+app = Flask(__name__)
+
+
+with open('frontend.html') as f:
+    frontend_template = f.read()
+
+@app.route('/')
+def index():
+    return frontend_template
+
+
+#@app.route('/classify', methods=['GET'])
+#def classify():
+#    inputmessage = request.args.get('inputmessage')
+#    return jsonify(format(predict(inputmessage)))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
