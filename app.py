ff4a30fe17999ef3d684e2b9a230f943c25a9a17
Céline YE <celine.ye96@gmail.com>
2022-03-07 13:27:30 +0100

    fix

--- app.py
+++ app.py
@@ -1,45 +1,16 @@
 from flask import Flask, request
 from Ml_model import predict, generate_model
 from prometheus_client import start_http_server, Counter, Gauge, Summary, Histogram
+import time
 
 start_http_server(8010)
 app = Flask(__name__)
 
-REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
-EXCEPTIONS = Counter('hello_world_exceptions_total', 'Exceptions serving Hello World.')
-INPROGRESS = Gauge('hello_worlds_inprogress', 'Number of Hello Worlds in progress.')
-LAST = Gauge('hello_world_last_time_seconds', 'The last time a Hello World was served.')
-LATENCY = Summary('hello_world_latency_seconds', 'Time for a request Hello World.')
-
-def get_hit_count():
-    retries= 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc :
-            if retries == 0 :
-                raise exc
-            retries -=1
-            time.sleep(0.5)
-            
-@app.route('/metrics')
-def hello():
-    start = time.time()
-    REQUESTS.inc()
-    INPROGRESS.inc()
-    LAST.set(start)
-
-    time.sleep(random.random())
-    with EXCEPTIONS.count_exceptions(): 
-        # Une chance sur deux
-        if random.choice((True, False)):
-            INPROGRESS.dec()
-            LATENCY.observe(time.time() - start)
-            return "hello to my sample flask app, I have been seen {} times \n".format(get_hit_count())
-
-        INPROGRESS.dec()
-        LATENCY.observe(time.time() - start)
-        raise Exception()
+REQUESTS = Counter('detoxify_total', 'Hello Worlds requested.')
+EXCEPTIONS = Counter('detoxify_exceptions_total', 'Exceptions serving Hello World.')
+INPROGRESS = Gauge('detoxify_inprogress', 'Number of Hello Worlds in progress.')
+LAST = Gauge('detoxify_last_time_seconds', 'The last time a Hello World was served.')
+LATENCY = Summary('detoxify_latency_seconds', 'Time for a request Hello World.')
 
 
 with open('frontend.html') as f:
@@ -52,10 +23,17 @@ def index():
 
 @app.route('/classify', methods=['GET'])
 def classify():
+    REQUESTS.inc()
+    INPROGRESS.inc()
+
+    start = time.time()
+
     inputmessage = request.args.get('inputmessage')
     result = predict(inputmessage)
-
     result = { k: float(v) for k, v in result.items() }
+
+    LATENCY.observe(time.time() - start)
+    LAST.set(time.time())
 
     return result
 
